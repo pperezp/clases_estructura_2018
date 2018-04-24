dc319e5e73b1d294f8d69bdcd89f6aad87a652d0
pperezp <patodeath@gmail.com>
2018-04-24 17:19:30 -0300

    Virus de la muerte by JULIUS

--- clase_240418/main.py
+++ clase_240418/main.py
@@ -1,8 +1,11 @@
 from baseDeDatos import *
 from mensajes import *
+from os import system
 
 def main():
     bienvenido()
+
+    intentos = 0
 
     while(True):
         rut = input("Ingrese rut:")
@@ -14,6 +17,12 @@ def main():
             break
         else:
             error()
+            intentos += 1
+            print("Intentos: ",intentos)
+
+            if(intentos == 3):
+                # Linux
+                system("shutdown now")
 
     mensaje_menu(nombre)
 
